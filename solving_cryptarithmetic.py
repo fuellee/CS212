6c4428e47db5c12d8f40c7ed931ee4e467f18f11
fuel_lee <lifuyu198919@gmail.com>
2013-10-16 21:41:43 +0800

    add

---
+++ solving_cryptarithmetic.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+# -------------
+# User Instructions
+#
+# Write a function, solve(formula) that solves cryptarithmetic puzzles.
+# The input should be a formula like 'ODD + ODD == EVEN', and the
+# output should be a string with the digits filled in, or None if the
+# problem is not solvable.
+#
+# Note that you will not be able to run your code yet since the
+# program is incomplete. Please SUBMIT to see if you are correct.
+from __future__ import division
+import string, re, itertools, time
+
+def solve(formula):
+    """Given a formula like 'ODD + ODD == EVEN', fill in digits to solve it.
+    Input formula is a string; output is a digit-filled-in string or None."""
+    # Your code here
+    for filled_f in fill_in(formula):
+        if valid(filled_f):
+            return filled_f
+
+def fill_in(formula):
+    "Generate all possible fillings-in of letters in formula with digits."
+    letters = ''.join(set(re.findall(r'[A-Z]',formula)))
+    for digits in itertools.permutations('1234567890',len(letters)):
+        table = string.maketrans(letters,''.join(digits))
+        yield formula.translate(table)
+        string.maketrans
+
+def valid(f):
+    """Formula f is valid if and only if it has no
+    numbers with leading zero, and evals true."""
+    try:
+        return not re.search(r'\b0[0-9]', f) and eval(f) is True
+    except ArithmeticError:
+        return False
+
+examples = """TWO + TWO == FOUR
+A**2 + B**2 == C**2
+A**2 + BE**2 == BY**2
+X / X == X
+A**N + B**N == C**N and N > 1
+ATOM**0.5 == A + TO + M
+GLITTERS is not GOLD
+ONE < TWO and FOUR < FIVE
+ONE < TWO < THREE
+RAMN == R**3 + RM**3 == N**3 + RX**3
+sum(range(AA)) == BB
+sum(range(POP)) == BOBO
+ODD + ODD == EVEN
+PLUTO not in set([PLANETS])""".splitlines()
+
+def timedcalls(n, fn, *args):
+    """Call fn(*args) repeatedly: n times if n is an int, or up to
+    n seconds if n is a float; return the min, avg, and max time"""
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        total = 0.0
+        while total < n:
+            t = timedcall(fn, *args)[0]
+            total += t
+            times.append(t)
+    return min(times), average(times), max(times)
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, result
+
+def test():
+    t0 = time.clock()
+    for example in examples:
+        print; print ' '*13, example
+        print '%6.4f sec:   %s '%timedcall(solve, example)
+    print '%6.4f tot.'%(time.clock()-t0)
+
+test()
