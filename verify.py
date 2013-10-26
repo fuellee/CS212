305aa1688b93e4ff04979f1178c457fbf2d8910f
fuel_lee <lifuyu198919@gmail.com>
2013-10-26 23:23:38 +0800

    add print_grammar

--- verify.py
+++ verify.py
@@ -1,17 +1,33 @@
 #! /usr/bin/env python
+
+def print_grammar(G):
+    for non_term in G:
+        dumped_rhs = ' | '.join([' '.join(alt)  for alt in G[non_term]])
+        print non_term,'=>',dumped_rhs
+
 def verify(G):
     lhs_tokens = set(G)-set(' ')
     rhs_tokens = set(token for alts in G.values() for alt in alts for token in alt if token.isalnum()) #can't handle '_'
     non_terms  = lhs_tokens
     terminals  = rhs_tokens-lhs_tokens
     orphans    = lhs_tokens-rhs_tokens  # Non-Terms that not in lhs
+    suspects   = [token for token in terminals if token.isalnum()]  # Terminals that looks like Non-Terms
 
     def show(title,tokens):
         print title,'=',' '.join(sorted(tokens))
+
+    def print_grammar():
+        for non_term in G:
+            [' '.join  for alt in G[non_term]]
+            print lhs,'=>',' | '.join([])
+
+
     show('Non-Terms', non_terms)  # G to lhs_tokens
     show('Terminals', terminals)  # should be regexes
-    show('Suspects', [token for token in terminals if token.isalnum()])  # Terminals that looks like Non-Terms
-    show('Orphans', orphans)
+    if suspects:
+        show('Suspects', suspects)
+    if orphans:
+        show('Orphans', orphans)
 
 if __name__ == '__main__':
     from grammar import G,grammar,parse
@@ -57,10 +73,11 @@ if __name__ == '__main__':
     xalphas => xalpha xalphas | xalpha
     xalpha => alpha | digit | safe | extra | escape
     """, whitespace = '()')
-    print "--URL grammar:\n",(URL)
-    print
-    verify(URL)
-    print
-    print "--parsed http://www.w3.org/Addressing/URL/5_BNF.html:\n",(parse('url', 'http://www.w3.org/Addressing/URL/5_BNF.html', URL))
+    # print "--URL grammar:\n",(URL)
+    # print
+    # verify(URL)
+    # print
+    # print "--parsed http://www.w3.org/Addressing/URL/5_BNF.html:\n",(parse('url', 'http://www.w3.org/Addressing/URL/5_BNF.html', URL))
+    print_grammar(URL)
 
 
