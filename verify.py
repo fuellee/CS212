57b52bf47a9701ad8266ef5c10bec0f540fc481d
fuel_lee <lifuyu198919@gmail.com>
2013-10-26 08:04:03 +0800

    add test cases

--- verify.py
+++ verify.py
@@ -14,5 +14,53 @@ def verify(G):
     show('Orphans', orphans)
 
 if __name__ == '__main__':
-    from grammar import G
+    from grammar import G,grammar,parse
     verify(G)
+    print "------------------"
+    ## Parsing URLs
+    ## See http://www.w3.org/Addressing/URL/5_BNF.html
+
+    URL = grammar("""
+    url => httpaddress | ftpaddress | mailtoaddress
+    httpaddress => http:// hostport /path? ?search?
+    ftpaddress => ftp:// login / path ; ftptype | ftp:// login / path
+    /path? => / path | ()
+    ?search? => [?] search | ()
+    mailtoaddress => mailto: xalphas @ hostname
+    hostport => host : port | host
+    host => hostname | hostnumber
+    hostname => ialpha . hostname | ialpha
+    hostnumber => digits . digits . digits . digits
+    ftptype => A formcode | E formcode | I | L digits
+    formcode => [NTC]
+    port => digits | path
+    path => void | segment / path | segment
+    segment => xalphas
+    search => xalphas + search | xalphas
+    login => userpassword hostport | hostport
+    userpassword => user : password @ | user @
+    user => alphanum2 user | alphanum2
+    password => alphanum2 password | password
+    path => void | segment / path | segment
+    void => ()
+    digits => digit digits | digit
+    digit => [0-9]
+    alpha => [a-zA-Z]
+    safe => [-$_@.&+]
+    extra => [()!*''""]
+    escape => % hex hex
+    hex => [0-9a-fA-F]
+    alphanum => alpha | digit
+    alphanums => alphanum alphanums | alphanum
+    alphanum2 => alpha | digit | [-_.+]
+    ialpha => alpha xalphas | alpha
+    xalphas => xalpha xalphas | xalpha
+    xalpha => alpha | digit | safe | extra | escape
+    """, whitespace = '()')
+    print "--URL grammar:\n",(URL)
+    print
+    verify(URL)
+    print
+    print "--parsed http://www.w3.org/Addressing/URL/5_BNF.html:\n",(parse('url', 'http://www.w3.org/Addressing/URL/5_BNF.html', URL))
+
+
